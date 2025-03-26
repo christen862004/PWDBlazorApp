935bb3b09b9f4215b6c685f3198f2ce8d42a85a6
Christen <christen.foad@gmail.com>
2025-03-26 12:38:00 +0200

    Add project files.

---
+++ PWDBlazorApp/Pages/DepartmentComponent.razor.cs
@@ -0,0 +1,28 @@
+﻿using PWDBlazorApp.Models;
+
+namespace PWDBlazorApp.Pages
+{
+    public partial class DepartmentComponent
+    {
+        public Department   Dept { get; set; }
+
+        public int DeptId { get; set; }
+
+        public List<Department> DeptList { get; set; }//=new List<Department>();
+
+        public DepartmentComponent()
+        {
+            DeptList = new List<Department>();
+            // Dept=new Department() { Id=1,Name="SD",ManagerName="Ahmed"};
+            DeptList.Add(new Department() { Id = 1, Name = "SD", ManagerName = "Ahmed" });
+            DeptList.Add(new Department() { Id = 2, Name = "OS", ManagerName = "Mohamed" });
+            DeptList.Add(new Department() { Id = 3, Name = "PW", ManagerName = "Sabreen" });
+            DeptList.Add(new Department() { Id = 4, Name = "UX", ManagerName = "Mariam" });
+        }
+
+        public void GetDEpt()
+        {
+            Dept= DeptList.FirstOrDefault(d => d.Id == DeptId);
+        }
+    }
+}
